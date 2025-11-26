9e3efb9b6ac45854110d6952716fe1d8e71461df
xuanhuyenitpct <xuanhuyenitpct@gmail.com>
2025-11-26 21:51:32 +0700

    Update QuizMasterApp.tsx

--- src/views/QuizMasterApp.tsx
+++ src/views/QuizMasterApp.tsx
@@ -1,3 +1,4 @@
+// @ts-nocheck
 import React, { useState, useEffect, useCallback } from "react";
 import { GoogleGenAI, Type } from "@google/genai";
 import { useApiKey } from '../context/ApiContext';
