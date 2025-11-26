17065e585d36a1016937f49312991c0dd9aa7edc
xuanhuyenitpct <xuanhuyenitpct@gmail.com>
2025-11-26 22:12:46 +0700

    chore: disable ts check in EnglishPracticeApp

--- src/views/EnglishPracticeApp.tsx
+++ src/views/EnglishPracticeApp.tsx
@@ -1,3 +1,4 @@
+// @ts-nocheck
 import React, { useState, useEffect, useRef } from "react";
 import { GoogleGenAI, Modality, Chat, Type } from "@google/genai";
 import { useApiKey } from '../context/ApiContext';
